a450cb18e8451be7a15f72a363e16f4aaa9a880f
pramod kumar <pramod.err@gmail.com>
2020-02-23 07:08:33 +0530

    add css file and access using express.static

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var express=require("express");
 
 var app=express();
 
+//_dirname is must for adding absolute path
+
+app.use(express.static('public'));
+//now any file in public folder may be used, just avoid public to add in url
+//expree.statis is mainly used to get css and other js file, the example shown in html file
 
 app.get("/",(req,res)=>{
 res.send("love u manisha");
 });
 
+//__dirname must to get the index file
 app.get("/king",(req,res)=>{
 res.sendFile(__dirname+"/public/test.html");
 });
