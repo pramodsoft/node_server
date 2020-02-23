f86f6862a4870e5a82993f5dc52b464ced1dc799
pramod kumar <pramod.err@gmail.com>
2020-02-23 07:28:13 +0530

    add a vue website in dist folder and run both website on same server

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app=express();
 app.use(express.static('public'));
 //now any file in public folder may be used, just avoid public to add in url
 //expree.statis is mainly used to get css and other js file, the example shown in html file
+app.use(express.static('public/dist'));
+
 
 app.get("/",(req,res)=>{
 res.send("love u manisha");
@@ -15,6 +17,10 @@ res.send("love u manisha");
 //__dirname must to get the index file
 app.get("/king",(req,res)=>{
 res.sendFile(__dirname+"/public/test.html");
+});
+
+app.get("/vue",(req,res)=>{
+res.sendFile(__dirname+"/public/dist/index.html");
 });
 
 app.listen(3000,()=>{
